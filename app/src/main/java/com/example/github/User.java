7697434b61368e597dea123f8e285e25d3a4d830
moathsaad <Moathabujazar@gmail.com>
2023-03-04 11:05:44 +0200

    Second commit

---
+++ app/src/main/java/com/example/github/User.java
@@ -0,0 +1,24 @@
+package com.example.github;
+
+public class User {
+    String id;
+    String username;
+    private  User(){}
+    User(String id, String username) {
+        this.id = id;
+        this.username = username;
+
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
